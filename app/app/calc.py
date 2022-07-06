5dbda0932d6283a71fb2e07422753a67c5b29c76
jonathanh21 <jhonathan.herrerao@gmail.com>
2022-07-06 13:19:02 -0500

    configuring database

--- app/app/calc.py
+++ app/app/calc.py
@@ -3,12 +3,13 @@ Calculator functions
 """
 
 
-def add(x ,y):
+def add(x, y):
 
     """ Add x abd y and return results."""
     return x+y
 
-def substract(x ,y):
-    
+
+def substract(x, y):
+
     """Substract x from y and return result"""
     return y-x
